69e7be3d84c6bc9960794fb0f4f66d15fae14fbe
Lukas <lukas.von.ehr@gmx.de>
2024-08-19 15:39:49 +0200

    first step in coloring

--- StarBattle Complete/Views/GridView.swift
+++ StarBattle Complete/Views/GridView.swift
@@ -5,12 +5,20 @@ struct GridView: View {
 
     var body: some View {
         // Berechne die Spalten direkt im body
-        let columns = Array(repeating: GridItem(.flexible()), count: viewModel.gridSize)
+        let columns = Array(repeating: GridItem(.flexible(), spacing: 0), count: viewModel.gridSize)
 
-        LazyVGrid(columns: columns, spacing: 10) {
-            ForEach(0..<viewModel.gridSize, id: \.self) { row in
-                ForEach(0..<viewModel.gridSize, id: \.self) { column in
-                    StarCellView(row: row, column: column)
+        ZStack {
+            // Schwarze Umrandung um das gesamte Projekt
+            Rectangle()
+                .stroke(Color.black, lineWidth: 2)
+            
+            LazyVGrid(columns: columns, spacing: 0) {
+                ForEach(0..<viewModel.gridSize, id: \.self) { row in
+                    ForEach(0..<viewModel.gridSize, id: \.self) { column in
+                        StarCellView(row: row, column: column)
+                            .background(Color.gray)
+                            .border(Color.black, width: 1) // Graue Balken und schwarze Umrandung zwischen Zellen
+                    }
                 }
             }
         }
