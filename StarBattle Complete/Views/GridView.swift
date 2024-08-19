65abddca6d1e7d676320bcaedac9030faf6f2866
Lukas <lukas.von.ehr@gmx.de>
2024-08-19 15:44:04 +0200

    coloring column separator

--- StarBattle Complete/Views/GridView.swift
+++ StarBattle Complete/Views/GridView.swift
@@ -8,19 +8,17 @@ struct GridView: View {
         let columns = Array(repeating: GridItem(.flexible(), spacing: 0), count: viewModel.gridSize)
 
         ZStack {
-            // Schwarze Umrandung um das gesamte Projekt
-            Rectangle()
-                .stroke(Color.black, lineWidth: 2)
-            
             LazyVGrid(columns: columns, spacing: 0) {
                 ForEach(0..<viewModel.gridSize, id: \.self) { row in
                     ForEach(0..<viewModel.gridSize, id: \.self) { column in
                         StarCellView(row: row, column: column)
                             .background(Color.gray)
-                            .border(Color.black, width: 1) // Graue Balken und schwarze Umrandung zwischen Zellen
+                            .border(Color.black, width: 0.5) // Dünnere schwarze Linien zwischen den Zellen
                     }
                 }
             }
+            .padding(0.5) // Dünnerer Abstand für die äußere Umrandung
+            .background(Color.black) // Schwarzer Rahmen um das gesamte Gitter
         }
     }
 }
